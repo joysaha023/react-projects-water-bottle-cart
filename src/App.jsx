6ee29844c2fa208b095a98bd5699b896da0a09b2
Joy <github.joysaha@gmail.com>
2024-03-12 10:08:05 +0600

    file created api data added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
-      <h1>Vite + React</h1>
+      <Header></Header>
       
       
     </>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Water Bottle Cart</h1>
+        </div>
+    );
+};
+
+export default Header;
