0ab8b9365e4456d371968c46baa2a28e950d04aa
Joy <github.joysaha@gmail.com>
2024-03-12 10:50:55 +0600

    show data and functionality added

---
+++ src/components/Bottle/Bottle.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Bottle = ({bottle}) => {
+    const {name, img, price} = bottle;
+    return (
+        <div>
+            <h3>Bottle: {name}</h3>
+            <img src={img} alt="" />
+            <p>Price: ${price}</p>
+        </div>
+    );
+};
+
+export default Bottle;
--- src/components/Bottoles/Bottols.jsx
+++ src/components/Bottoles/Bottols.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Bottle from '../Bottle/Bottle';
 
 const Bottols = () => {
+    const [bottles, setBottles] = useState([])
+
+    useEffect(() =>{
+        fetch('bottole.json')
+        .then(res => res.json())
+        .then(data => setBottles(data))
+    },[])
+
+
     return (
         <div>
-            <h3>Hello Bottles</h3>
+            <h3>Hello Bottles {bottles.length}</h3>
+            {
+                bottles.map((item) => <Bottle key={item.id} bottle={item}></Bottle>)
+            }
         </div>
     );
 };
